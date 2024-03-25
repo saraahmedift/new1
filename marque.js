3e42a323d05c2f4aea9a2ffaaacfffc17f49c1ac
saraahmedift <164875918+saraahmedift@users.noreply.github.com>
2024-03-25 16:56:32 +0500

    Add files via upload

---
+++ marque.js
@@ -0,0 +1,9 @@
+function marqueStudents() {
+    var totalStudents = 900;
+    var compStudents = 150;
+    var laptopUsers = totalStudents - compStudents;
+    return laptopUsers;
+}
+report;
+defaults;
+marqueStudents;
